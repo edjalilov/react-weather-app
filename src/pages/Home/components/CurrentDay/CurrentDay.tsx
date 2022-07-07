ac85cd20d75dbb9c5553e63a1caf5465d1ab268c
Elyor Djalilov <edjalilov.dev@gmail.com>
2022-07-08 00:18:08 +0500

    minor changes

--- src/pages/Home/components/CurrentDay/CurrentDay.tsx
+++ src/pages/Home/components/CurrentDay/CurrentDay.tsx
@@ -7,18 +7,24 @@ interface Props {
 }
 
 export const CurrentDay = ({ weather }: Props) => {
+  const today = new Date();
+  const currentTime =
+    today.getHours() + ":" + today.getMinutes()
+
   return (
     <div className={style.current_day}>
       <div className={style.top_block}>
         <div className={style.top_block_wrapper}>
-          <div className={style.current_temp}>{Math.floor(weather.main.temp)}°</div>
+          <div className={style.current_temp}>
+            {Math.floor(weather.main.temp)}°
+          </div>
           <div className={style.this_day}>Сегодня</div>
         </div>
         <GlobalSvgSelector id="sun" />
       </div>
       <div className={style.bottom_block}>
         <div className={style.current_time}>
-          Время: <span>1:28</span>
+          Время: <span>{currentTime}</span>
         </div>
         <div className={style.current_city}>
           Город: <span>Ташкент</span>
